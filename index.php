6ef2d1dd1f5b89dbd9ba59d4dfae9dc6ddc6fda6
Niktux <nico.ln@gmail.com>
2014-02-16 16:17:14 +0100

    Use symfony console

--- index.php
+++ index.php
@@ -1,47 +1,12 @@
 <?php
 
-use Gaufrette\Filesystem;
-use Gaufrette\Adapter\Local;
-use Gaufrette\File;
-use Solidifier\Visitors\Property\PublicAttributes;
-use Solidifier\Visitors\GetterSetter\FluidSetters;
-
 require 'vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Solidifier\Command\Run;
 
 ini_set('xdebug.max_nesting_level', 250);
 
-$adapter = new Local('../karma/src');
-$fs = new Filesystem($adapter);
-
-foreach($fs->keys() as $key)
-{
-    if($adapter->isDirectory($key) === false)
-    {
-        parseFile($fs->get($key));
-    }
-}
-
-function parseFile(File $file)
-{
-    $key = $file->getKey();
-  //  echo "\n*** In file $key\n";
-    
-    $code = $file->getContent();
-    
-    $parser = new PhpParser\Parser(new PhpParser\Lexer());
-    $traverser     = new PhpParser\NodeTraverser();
-    
-    $traverser->addVisitor(new PublicAttributes());
-    $traverser->addVisitor(new FluidSetters());
-    
-    try
-    {
-        $stmts = $parser->parse($code);
-        
-        $traverser->traverse($stmts);
-    }
-    catch (PhpParser\Error $e)
-    {
-        echo 'Parse Error: ', $e->getMessage();
-    }
-}
+$app = new Application();
+$app->add(new Run());
+$app->run();
---
+++ lib/Solidifier/Command/Run.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Solidifier\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Gaufrette\Adapter\Local;
+use Gaufrette\Filesystem;
+use Gaufrette\File;
+use PhpParser\Parser;
+use PhpParser\Lexer;
+use PhpParser\NodeTraverser;
+use Solidifier\Visitors\Property\PublicAttributes;
+use Solidifier\Visitors\GetterSetter\FluidSetters;
+
+class Run extends Command
+{
+    protected function configure()
+    {
+        $this->setName('run')
+            ->setDescription('Check rules')
+            ->addArgument('src', InputArgument::REQUIRED, 'sources to parse');
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $src = $input->getArgument('src');
+
+        $adapter = new Local($src);
+        $fs = new Filesystem($adapter);
+        
+        foreach($fs->keys() as $key)
+        {
+            if($adapter->isDirectory($key) === false)
+            {
+                $this->parseFile($fs->get($key));
+            }
+        }
+    }
+
+    private function parseFile(File $file)
+    {
+        $key = $file->getKey();
+        $code = $file->getContent();
+    
+        $parser = new Parser(new Lexer());
+        $traverser = new NodeTraverser();
+    
+        $traverser->addVisitor(new PublicAttributes());
+        $traverser->addVisitor(new FluidSetters());
+    
+        try
+        {
+            $stmts = $parser->parse($code);
+            $traverser->traverse($stmts);
+        }
+        catch (PhpParser\Error $e)
+        {
+            $output->writeln('Parse Error: ', $e->getMessage());
+        }
+    }
+}
