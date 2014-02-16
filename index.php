96bf936cedb8782703901368e2c50c1632d2ccbd
Niktux <nico.ln@gmail.com>
2014-02-16 15:51:20 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+use Solidifier\Visitors\PublicAttributes;
+use Gaufrette\Filesystem;
+use Gaufrette\Adapter\Local;
+use Gaufrette\File;
+use Solidifier\Visitors\FluidSetters;
+
+require 'vendor/autoload.php';
+
+ini_set('xdebug.max_nesting_level', 250);
+
+
+$adapter = new Local('../karma/src');
+$fs = new Filesystem($adapter);
+
+foreach($fs->keys() as $key)
+{
+    if($adapter->isDirectory($key) === false)
+    {
+        parseFile($fs->get($key));
+    }
+}
+
+
+function parseFile(File $file)
+{
+    $key = $file->getKey();
+  //  echo "\n*** In file $key\n";
+    
+    $code = $file->getContent();
+    
+    $parser = new PhpParser\Parser(new PhpParser\Lexer());
+    $traverser     = new PhpParser\NodeTraverser();
+    
+    $traverser->addVisitor(new PublicAttributes());
+    $traverser->addVisitor(new FluidSetters());
+    
+    try
+    {
+        $stmts = $parser->parse($code);
+        
+        $traverser->traverse($stmts);
+    }
+    catch (PhpParser\Error $e)
+    {
+        echo 'Parse Error: ', $e->getMessage();
+    }
+
+}
