b05a56f693572f9ad8595cc202f8f41196df0ae0
Niktux <nico.ln@gmail.com>
2014-02-22 20:22:13 +0100

    Visitor StrongCoupling unit tests

---
+++ tests/Solidifier/Visitors/DependencyInjection/StrongCouplingTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Solidifier\Visitors\DependencyInjection;
+
+use Solidifier\AnalyzeTestCase;
+use Solidifier\Analyzer;
+
+class StrongCouplingTest extends AnalyzeTestCase
+{
+    const
+        DEFECT_TYPE = 'Solidifier\Defects\StrongCoupling';
+    
+    private
+        $visitor;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $this->visitor = new StrongCoupling();
+    }
+    
+    protected function configureAnalyzer(Analyzer $analyzer)
+    {
+        $analyzer->addVisitor('analyze', $this->visitor);
+    }
+    
+    public function testSimple()
+    {
+        $this->analyze(array(
+            'foo.php' => '<?php
+            class Foo
+            {
+                public function bar()
+                {
+                    $a = new Baz();
+                }
+            }',
+        ));
+    
+        $this->assertContainsType(self::DEFECT_TYPE, 1);
+    }
+    
+    public function testOutsideClass()
+    {
+        $this->analyze(array(
+            'foo.php' => '<?php
+            $f = new Foo();                        
+            class Foo
+            {
+            }',
+        ));
+    
+        $this->assertContainsType(self::DEFECT_TYPE, 0);
+    }
+    
+    public function testMagical()
+    {
+        $this->analyze(array(
+            'foo.php' => '<?php
+            class Foo
+            {
+                public function bar()
+                {
+                    $a = new $baz();
+                }
+            }',
+        ));
+    
+        $this->assertContainsType(self::DEFECT_TYPE, 0);
+    }   
+    
+    public function testExclude()
+    {
+        $this->visitor->addExcludePattern('~Baz$~');
+        
+        $this->analyze(array(
+            'foo.php' => '<?php
+            class Foo
+            {
+                public function bar()
+                {
+                    $a = new Bar();
+                    $b = new Baz();
+                }
+            }',
+        ));
+    
+        $this->assertContainsType(self::DEFECT_TYPE, 1);
+    }   
+    
+    public function testNamespace()
+    {
+        $this->analyze(array(
+            'foo.php' => '<?php
+            class Foo
+            {
+                public function bar()
+                {
+                    $a = new Some\NameSpaced\Classname\Bar();
+                    $b = new Baz();
+                }
+            }',
+        ));
+    
+        $this->assertContainsType(self::DEFECT_TYPE, 2);
+    }   
+    
+    public function testNamespaceAndExclude()
+    {
+        $this->visitor->addExcludePattern('~Iterator~');
+        
+        $this->analyze(array(
+            'foo.php' => '<?php
+            class Foo
+            {
+                public function bar()
+                {
+                    $a = new Some\NameSpaced\Classname\BarIterator();
+                    $b = new Iterators\Baz();
+                }
+            }',
+        ));
+    
+        // exclude pattern must only apply on classnames, not namespace
+        $this->assertContainsType(self::DEFECT_TYPE, 1);
+    }   
+}
