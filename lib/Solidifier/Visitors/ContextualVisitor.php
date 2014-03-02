4850e1d6dafe0353b4395cd76e385e59eeb33089
Niktux <nico.ln@gmail.com>
2014-03-03 00:55:04 +0100

    Add currentMethod in context

--- lib/Solidifier/Visitors/ContextualVisitor.php
+++ lib/Solidifier/Visitors/ContextualVisitor.php
@@ -8,17 +8,20 @@ use PhpParser\Node\Stmt\Interface_;
 use PhpParser\Node\Stmt\Trait_;
 use PhpParser\Node\Stmt\Namespace_;
 use Solidifier\Dispatcher;
+use PhpParser\Node\Stmt\ClassMethod;
 
 abstract class ContextualVisitor extends AbstractVisitor
 {
     protected
         $currentNamespace,
-        $currentObjectType;
+        $currentObjectType,
+        $currentMethod;
 
     public function beforeTraverse(array $nodes)
     {
         $this->currentNamespace = null;
         $this->currentObjectType = null;
+        $this->currentMethod = null;
     }
     
     public function enterNode(Node $node)
@@ -39,6 +42,10 @@ abstract class ContextualVisitor extends AbstractVisitor
         {
             $this->currentObjectType = new ObjectType($this->currentNamespace, $node->name, ObjectType::TYPE_TRAIT);
         }
+        elseif($node instanceof ClassMethod)
+        {
+            $this->currentMethod = $node->name;
+        }
     }
     
     public function leaveNode(Node $node)
@@ -47,9 +54,13 @@ abstract class ContextualVisitor extends AbstractVisitor
         {
             $this->currentNamespace = null;
         }
-        else if($node instanceof Class_ || $node instanceof Interface_ || $node instanceof Trait_)
+        elseif($node instanceof Class_ || $node instanceof Interface_ || $node instanceof Trait_)
         {
             $this->currentObjectType = null;
+        }
+        elseif($node instanceof ClassMethod)
+        {
+            $this->currentMethod = null;
         }
     }
 }
--- lib/Solidifier/Visitors/GetterSetter/FluidSetters.php
+++ lib/Solidifier/Visitors/GetterSetter/FluidSetters.php
@@ -5,7 +5,6 @@ namespace Solidifier\Visitors\GetterSetter;
 use Solidifier\Visitors\ContextualVisitor;
 use Solidifier\Visitors\ObjectType;
 use PhpParser\Node;
-use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\Expr\Variable;
 use Solidifier\Defects\NotFluidSetter;
@@ -13,38 +12,37 @@ use Solidifier\Defects\NotFluidSetter;
 class FluidSetters extends ContextualVisitor
 {
     private
-        $currentMethod;
+        $currentMethodState;
     
     public function beforeTraverse(array $nodes)
     {
         parent::beforeTraverse($nodes);
         
-        $this->currentMethod = null;
+        $this->currentMethodState = null;
     }
     
     public function enterNode(Node $node)
     {
         parent::enterNode($node);
         
-        // FIXME callmethod stack ...
         if($node instanceof ClassMethod)
         {
             if(strtolower(substr($node->name, 0, 3)) === 'set')
             {
-                $this->currentMethod = new FluidSetterState($node->name);
+                $this->currentMethodState = new FluidSetterState($node->name);
             }
         }
         elseif($node instanceof Return_)
         {
-            if($this->currentMethod instanceof FluidSetterState)
+            if($this->currentMethodState instanceof FluidSetterState)
             {
-                $this->currentMethod->returnCount++;
+                $this->currentMethodState->returnCount++;
                 
                 if($node->expr instanceof Variable)
                 {
                     if($node->expr->name === 'this')
                     {
-                        $this->currentMethod->returnThis = true;
+                        $this->currentMethodState->returnThis = true;
                     }        
                 }                
             }
@@ -57,10 +55,10 @@ class FluidSetters extends ContextualVisitor
         
         if($node instanceof ClassMethod)
         {
-            if($this->currentMethod instanceof FluidSetterState)
+            if($this->currentMethodState instanceof FluidSetterState)
             {
                 if($this->currentObjectType->type !== ObjectType::TYPE_INTERFACE
-                && $this->currentMethod->isValid() === false)
+                && $this->currentMethodState->isValid() === false)
                 {
                     $this->dispatch(
                        new NotFluidSetter($this->currentObjectType, $node)
@@ -68,7 +66,7 @@ class FluidSetters extends ContextualVisitor
                 }
             }
             
-            $this->currentMethod = null;
+            $this->currentMethodState = null;
         }
     }
 }
