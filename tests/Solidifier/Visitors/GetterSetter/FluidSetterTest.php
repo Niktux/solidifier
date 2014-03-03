ae4c87a3fa38a695509d95e2ab8d3df0ff846066
Niktux <nico.ln@gmail.com>
2014-03-03 01:15:52 +0100

    #11 Add visitor FluidSetter unit tests

---
+++ tests/Solidifier/Visitors/GetterSetter/FluidSetterTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Solidifier\Visitors\GetterSetter;
+
+use Solidifier\AnalyzeTestCase;
+use Solidifier\Analyzers\Analyzer;
+
+class FluidSetterTest extends AnalyzeTestCase
+{
+    const
+        DEFECT_TYPE = 'Solidifier\Defects\NotFluidSetter';
+    
+    private
+        $visitor;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $this->visitor = new FluidSetters();
+    }
+    
+    protected function configureAnalyzer(Analyzer $analyzer)
+    {
+        $analyzer->addVisitor('analyze', $this->visitor);
+    }
+    
+    public function testSimple()
+    {
+        $this->analyze(array(
+            'foo.php' => '<?php
+            class Foo
+            {
+                private $bar;
+                        
+                public function setBar(Bar $b)
+                {
+                    $this->bar = $bar;
+                }
+            }',
+        ));
+    
+        $this->assertContainsType(self::DEFECT_TYPE, 1);
+    }
+    
+    public function testNegative()
+    {
+        $this->analyze(array(
+            'foo.php' => '<?php
+            class Foo
+            {
+                private $bar;
+                        
+                // Not a named as a setter
+                public function initializeBar(Bar $b)
+                {
+                    $this->bar = $bar;
+                }
+                        
+                // Fluid setter
+                public function setBar(Bar $b)
+                {
+                    $this->bar = $bar;
+                    
+                    return $this;
+                }    
+
+                // tricky name
+                public function unsetBar() {}
+            }',
+        ));
+    
+        $this->assertContainsType(self::DEFECT_TYPE, 0);
+    }
+    
+    public function testMultipleReturnStatements()
+    {
+        $this->analyze(array(
+            'foo.php' => '<?php
+            class Foo
+            {
+                private $bar;
+                        
+                public function setBar(Bar $b)
+                {
+                    if($b->a === null)
+                    {
+                        return false;
+                    }
+                        
+                    $this->bar = $bar;
+                    
+                    return $this;
+                }                        
+            }',
+        ));
+    
+        $this->assertContainsType(self::DEFECT_TYPE, 1);
+    }
+    
+    public function testBadReturnStatements()
+    {
+        $this->analyze(array(
+            'foo.php' => '<?php
+            class Foo
+            {
+                private $bar;
+                        
+                public function setBar(Bar $b)
+                {
+                    $this->bar = $bar;
+                    
+                    return false;
+                }                        
+            }',
+        ));
+    
+        $this->assertContainsType(self::DEFECT_TYPE, 1);
+    }
+}
