71fa905efe048f6b05d5651378cbf7338f20854a
Niktux <nico.ln@gmail.com>
2014-05-29 12:48:05 +0200

    CS

--- lib/Solidifier/Reporters/XMLReporter.php
+++ lib/Solidifier/Reporters/XMLReporter.php
@@ -14,31 +14,31 @@ class XMLReporter implements Reporter
         $this->dom = new \DOMDocument('1.0', 'utf-8');
     }
     
-	public function render(array $defects)
-	{
-	    $root = $this->dom->createElement('project');
-	    $this->dom->appendChild($root);
-	    
-	    $node = $this->dom->createElement('defects', $this->countDefects($defects));
-	    $root->appendChild($node);
-	    
-	    return $this;
-	}
-	
-	private function countDefects($defects)
-	{
-	    $count = 0;
-	    
-	    foreach($defects as $fileDefects)
-	    {
-	        $count += count($fileDefects);
-	    }
-	    
-	    return $count;
-	}
+    public function render(array $defects)
+    {
+        $root = $this->dom->createElement('project');
+        $this->dom->appendChild($root);
+        
+        $node = $this->dom->createElement('defects', $this->countDefects($defects));
+        $root->appendChild($node);
+        
+        return $this;
+    }
+    
+    private function countDefects($defects)
+    {
+        $count = 0;
+        
+        foreach($defects as $fileDefects)
+        {
+            $count += count($fileDefects);
+        }
+        
+        return $count;
+    }
 
-	public function save($reportFilename)
-	{
-	    file_put_contents($reportFilename, $this->dom->saveXML());
-	}
+    public function save($reportFilename)
+    {
+        file_put_contents($reportFilename, $this->dom->saveXML());
+    }
 }
